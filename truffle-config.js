cc4959e6bd073397a8ba2094f80fd16d6741ac6f
Rayyan <rayyan808@gmail.com>
2021-05-06 18:38:52 +0200

    Truffle File Edited to 0.8.0

--- truffle-config.js
+++ truffle-config.js
@@ -2,6 +2,11 @@ module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // for more about customizing your Truffle configuration!
   networks: {
+    compilers: {
+     solc: {
+       version: "0.8.0" 
+     }
+    },
     development: {
       host: "127.0.0.1",
       port: 7545,
